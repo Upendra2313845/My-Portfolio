1e3907561da9be51e66405cb71a050975525bec2
Upendra2313845 <2k23.cs2313845@gmail.com>
2025-11-14 21:47:33 +0530

    push my updated code

--- script.js
+++ script.js
@@ -5,7 +5,7 @@
 (function() {
   const phrases = [
     "Full Stack Development",
-    "Machine Learning",
+    // "Machine Learning",
     "Data Science",
     "MERN Stack"
   ];
@@ -53,4 +53,68 @@
   // small delay before starting for nicer effect
   setTimeout(tick, 500);
 })();
+
+
+
+// MOBILE MENU TOGGLE
+const menuToggle = document.getElementById('menuToggle');
+const menu = document.getElementById('menu');
+
+menuToggle.addEventListener('click', () => {
+    menu.classList.toggle('active');
+});
+
+// NAVBAR SCROLL EFFECT
+const header = document.querySelector('header');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 50) {
+        header.classList.add('header-scrolled');
+    } else {
+        header.classList.remove('header-scrolled');
+    }
+});
+
+
+
+
+
+
+const track = document.getElementById("track");
+const loop = document.getElementById("loop");
+
+// 1) Duplicate skills for infinite loop
+track.innerHTML += track.innerHTML;
+
+// DRAG SCROLL
+let isDown = false;
+let startX;
+let scrollLeft;
+
+loop.addEventListener('mousedown', (e) => {
+    isDown = true;
+    startX = e.pageX;
+    scrollLeft = loop.scrollLeft;
+});
+loop.addEventListener('mouseup', () => isDown = false);
+loop.addEventListener('mouseleave', () => isDown = false);
+
+loop.addEventListener('mousemove', (e) => {
+    if (!isDown) return;
+    e.preventDefault();
+    const walk = (e.pageX - startX) * 2;
+    loop.scrollLeft = scrollLeft - walk;
+});
+
+// 3) Infinite Loop Reset (magic)
+loop.addEventListener('scroll', () => {
+    let maxScroll = track.scrollWidth / 2;
+
+    if (loop.scrollLeft >= maxScroll) {
+        loop.scrollLeft = 1;
+    } 
+    else if (loop.scrollLeft <= 0) {
+        loop.scrollLeft = maxScroll - 1;
+    }
+});
 
